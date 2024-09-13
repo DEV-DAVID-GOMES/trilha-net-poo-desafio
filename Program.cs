8457b24f91f049515679475dae9dc0ff16561821
DEV-DAVID-GOMES <168876315+DEV-DAVID-GOMES@users.noreply.github.com>
2024-09-13 15:46:03 -0300

    primeiro commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone nokia");
+Console.WriteLine("");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Nokia 3310", imei: "123456789", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+Console.WriteLine("-------------------------------------------");
+Console.WriteLine("Smartphone iphone");
+Console.WriteLine("");
+Iphone iphone = new Iphone(numero: "987654", modelo: "iPhone 13",  imei: "987654321", memoria: 128);
+nokia.ReceberLigacao();
+iphone.InstalarAplicativo("facebook");
+Console.WriteLine("-------------------------------------------");
+
